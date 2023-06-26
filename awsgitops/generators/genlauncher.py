9754541ec98bc668432125a5c6a2f9d144fd2c60
bczemp <ben@munirvana.com>
2023-06-26 01:38:51 -0400

    Add a status enum, limit status table to enum, isProvisioned -> getInstance, and isWired and isValid combined

--- awsgitops/generators/genlauncher.py
+++ awsgitops/generators/genlauncher.py
@@ -1,12 +1,22 @@
 from threading import Lock, Thread
 from copy import deepcopy
 import importlib
+from enum import Enum
+
+# Class for the various status categories
+class Status(Enum):
+     STATUS = 1
+     GET_INST = 2
+     OPERATIONAL = 3
+     GET_DATA = 4
+     GENERATE = 5
+     FAILED = 6
 
 # Dynamically load generator classes and autopopulate the status dict
 def load_generators(generator_config):
     generators = {module_name: getattr(importlib.import_module(f"awsgitops.generators.{module_name}"), module_name) for module_name in generator_config.keys() if module_name != "CONFIG"}
 
-    status = {"Status": "Not Started", "isProvisioned": "", "isWired": "", "isValid": "", "getData": "", "generateData": "", "FAILED": False}
+    status = {Status.STATUS: "Not Started", Status.GET_INST: "", Status.OPERATIONAL: "", Status.GET_DATA: "", Status.GENERATE: "", Status.FAILED: False}
     statuses = {generator: deepcopy(status) for generator in generators.keys() if generator != "CONFIG"}
 
     return generators, statuses
