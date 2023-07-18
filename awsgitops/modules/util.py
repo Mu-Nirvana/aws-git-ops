813dc769ee7893f7ee0ca2bdea93d4baa6c7ad0a
bczemp <ben@munirvana.com>
2023-07-18 14:46:49 -0400

    Added search function

--- awsgitops/modules/util.py
+++ awsgitops/modules/util.py
@@ -10,6 +10,7 @@ def read(yaml, *keys):
     else:
         return read(yaml[keys[0]], *keys[1:])
 
+# Check if a key is present in a yaml object
 def is_present(yaml, *keys):
     if len(keys) == 1:
         if keys[0] in yaml: return True
@@ -18,6 +19,21 @@ def is_present(yaml, *keys):
         if keys[0] not in yaml: return False
         return read(yaml[keys[0]], *keys[1:])
 
+# Find the path to a key in a yaml object
+def find(yaml, key, path=[]):
+    matches = []
+    if type(yaml) != dict and type(yaml) != list:
+        return matches 
+    for x, item in enumerate(yaml):
+        if item == key:
+            matches.append(path + [key])
+            return matches 
+        if type(yaml) == dict:
+            result = find(yaml[item], key, path=path+[item])
+        else:
+            result = find(yaml[x], key, path=path+[x])
+        matches += result
+    return matches
 
 # Write a value to a yaml object with a variable number of keys
 def write(yaml, value, *keys):
